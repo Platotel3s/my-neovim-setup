1f1df38204ac0c6c1268695b8d7bc573a35d9de6
Platotel3s <yudhaplato@gmail.com>
2026-01-29 03:55:21 +0700

    third

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -167,13 +167,4 @@ vim.lsp.lua_ls={
 }
 vim.lsp.enable("lua_ls")
 
--- -----------------------------------------------------
--- -- EJS
--- -----------------------------------------------------
--- vim.lsp.emmet_ls={
---   on_attach=on_attach(),
---   filetypes={
---     "ejs","javascript","html"
---   }
--- }
--- vim.lsp.enable("emmet_ls")
+
